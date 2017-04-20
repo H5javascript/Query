cdebc47bbacc3ec54246e97a61a341e135b4e4cd
dtimmer <timmer@bcodey.com>
2017-04-20 15:46:03 +0800

    新增中间件式编程

--- $.js
+++ $.js
@@ -937,3 +937,37 @@ var ajax = function () {
   }
 
 }()
+
+
+/*中间件式编程*/
+function Middleware(){
+  this.cache = [];
+  this.options = null;//缓存options
+}
+
+Middleware.prototype.use = function(fn){
+  if(typeof fn !== 'function'){
+    throw 'middleware must be a function';
+  }
+  this.cache.push(fn);
+  return this;
+}
+
+Middleware.prototype.next = function(fn){
+
+  if(this.middlewares && this.middlewares.length > 0 ){
+    var ware = this.middlewares.shift();
+    ware.call(this, this.options, this.next.bind(this));//传入options与next
+  }
+}
+/**
+* @param options 数据的入口
+* @param next 
+*/
+Middleware.prototype.handleRequest = function(options){
+  this.middlewares = this.cache.map(function(fn){//复制
+    return fn;
+  });
+  this.options = options;//缓存数据
+  this.next();
+}
